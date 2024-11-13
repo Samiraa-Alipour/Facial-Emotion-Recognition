09959a4e6e19f8c07e77bfbcac576ceea3abfe68
Samiraa-Alipour <samiraalipour1989@gmail.com>
2024-11-13 16:14:50 +0330

    Ensure correct port binding

--- app.py
+++ app.py
@@ -306,4 +306,5 @@ def process_image():
 
 if __name__ == '__main__':
 #    app.run(debug=True)
-   app.run(host='0.0.0.0', port=os.getenv('PORT', 8080))
+#    app.run(host='0.0.0.0', port=os.getenv('PORT', 8080))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
